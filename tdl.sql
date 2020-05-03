4d18a02b49651229fcea7be46e5acab37f039f67
julia-denivet <julia.denivet@laplateforme.io>
2020-05-03 18:20:50 +0200

    commit bdd

---
+++ tdl.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 03 mai 2020 à 16:20
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tdl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `liste`
+--
+
+DROP TABLE IF EXISTS `liste`;
+CREATE TABLE IF NOT EXISTS `liste` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `list` varchar(255) NOT NULL,
+  `id_utilisateurs` int(11) NOT NULL,
+  `date_creation` date NOT NULL,
+  `date_fin` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `liste`
+--
+
+INSERT INTO `liste` (`id`, `list`, `id_utilisateurs`, `date_creation`, `date_fin`) VALUES
+(2, 'Finir Unit 3 ', 1, '2020-05-05', '2020-05-06');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
